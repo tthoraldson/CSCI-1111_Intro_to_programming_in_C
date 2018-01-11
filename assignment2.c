296a755684b80f9e5c3f661ae95ee8e26629acac
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2018-01-11 11:27:17 -0600

    assignment2.c complete

---
+++ assignment2.c
@@ -0,0 +1,33 @@
+// Assignment #2 - CSCI-1111
+// Theresa Thoraldson 1/11/2018
+
+#include <stdio.h>
+int main()
+{
+    // Variables
+    char name[] = "Theresa Thoraldson";
+    char email[] = "theresa.thoraldson@gmail.com";
+    char highSchool[] = "Perpich Center for Arts Education";
+    char homeTown[] = "Bloomington, MN";
+    char currentCollege[] = "Normandale";
+    char currentYearInSchool[] = "College Freshman";
+    int age = 22;
+
+    // Print Statements
+    printf("                   Name : %s\n", name);
+    printf("                 E-mail : %s\n", email);
+    printf("            High School : %s\n", highSchool);
+    printf("              Home town : %s\n", homeTown);
+    printf("        Current College : %s\n", currentCollege);
+    printf(" Current year in school : %s\n", currentYearInSchool);
+    printf("                    Age : %i\n", age);
+    printf("           Age in octal : %o\n", age);
+    printf("     Age in hexadecimal : %X\n", age);
+
+    // Return Statement
+    return 0;
+}
+
+/* OUTPUT: assignment2.c
+
+*/
